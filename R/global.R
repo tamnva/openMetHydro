efb309b05d6d04f9d0e3f8c33da95758d684ef0e
Tam Nguyen <tamnva@gmail.com>
2025-04-11 21:25:52 +0200

    update global

--- R/global.R
+++ R/global.R
@@ -4,6 +4,11 @@ library(sf)
 library(dplyr)
 library(rdwd)
 
+library(terra)
+library(sf)
+library(exactextractr)
+library(data.table)
+
 # Load spatial data - using US basins shapefile from 'sf' package
 setwd("C:/Users/nguyenta/Documents/GitHub/openMetHydro")
 basins <- st_read(file.path("data", "catchments.shp"), quiet = TRUE)
@@ -16,23 +21,84 @@ stations <- st_transform(stations, '+proj=longlat +datum=WGS84')
 #stations <- stations[c(1:10),]
 
 # Download germany data
-get_dwd_data <- function(years, folder, catchments){
+get_dwd_hyras <- function(years, save_dir){
   
-  dir.create(file.path("data", "temp_dir"), showWarnings = FALSE)
-  base_link <- "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/xxxx_hyras_1_yyyy_v6-0_de.nc"
+  #save_dir <- "C:/Users/nguyenta/Documents/GitHub/openMetHydro/data/temp_dir"
+  #years <- c(1980:2025)
   
-  for (year in years){
-    for (data in c("precipitation/pr", 
-                   "air_temperature_min/tasmin", 
-                   "air_temperature_min/tasmin")){
-      link <- gsub("xxxx", data, base_link)
-      link <- gsub("yyyy", year, link)
-      download.file(url = link, file.path("data", "temp_dir", basename(link)))
+  options(timeout=3600)
+  
+  base_link <- "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de"
+  file_name <- "_hyras_1_year_v6-0_de.nc"
+  
+  base_link_suffix <- c("precipitation", "air_temperature_min", "air_temperature_max", "humidity")
+  file_name_prefix <- c("pr", "tasmin", "tasmax", "hurs")
+  
+  for (yr in years){
+    for (i in c(1:4)){
+      
+      base_link_update <- file.path(base_link, base_link_suffix[i])
+      file_name_update <- paste0(file_name_prefix[i], file_name)
+      file_name_update <- gsub("year", yr, file_name_update)
+      
+      download.file(file.path(base_link_update, file_name_update), 
+                    file.path(save_dir,file_name_update),
+                    mode="wb")
+
     }
   }
   
-  #nwpbase <- "ftp://opendata.dwd.de/weather/nwp/icon-d2/grib/00/t_2m"
-  #links <- indexFTP("", base=nwpbase, dir=tempdir())
-  #file <- dataDWD(links[6], base=nwpbase, joinbf=TRUE, dir=tempdir(), read=FALSE)
-  #forecast <- readDWD(file)
 }
+
+
+basin_extraction_hyras <- function(years, data_dir, basin_shp, out_dir=NA){
+  
+  if (is.na(out_dir)) out_dir <- data_dir
+  
+  file_name <- "_hyras_1_year_v6-0_de.nc"
+  file_name_prefix <- c("pr", "tasmin", "tasmax", "hurs")
+  
+  for (yr in years){
+    
+    for (i in c(1:4)){
+      
+      message(yr)
+      
+      file_name_update <- file.path(
+        data_dir, gsub("year", yr, paste0(file_name_prefix[i], file_name)))
+      
+      # Get data
+      data <- rast(file_name_update)
+
+      if (yr == years[1]) basin_shp <- st_transform(basin_shp, crs(data))
+      
+      # Extract data
+      data <- exact_extract(data, 
+                    basin_shp,   #subset(basin_shp, gauge_id == "DE110000"), 
+                    fun = 'mean')
+      
+      data_extract <- t(data.frame(data))
+      colnames(data_extract) <- basin_shp$gauge_id
+      
+      if (yr == years[1]) {
+        write.table(x = data_extract, 
+                  file = file.path(out_dir, paste0(file_name_prefix[i], ".csv")), 
+                  row.names = FALSE, sep = ",", quote = FALSE)
+        
+      } else {
+        write.table(x = data_extract,
+                    file = file.path(out_dir, paste0(file_name_prefix[i], ".csv")), 
+                    row.names = FALSE, sep = ",", quote = FALSE, append = TRUE)
+      }
+    }
+  }
+  
+}
+
+
+#data_dir <- "C:/Users/nguyenta/Documents/GitHub/openMetHydro/data/temp_dir"
+#years <- c(1980:2025)
+#basin_shp <- basins
+#out_dir <- data_dir
+
+
